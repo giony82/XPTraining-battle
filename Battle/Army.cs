9723ccb9104f8567ea15b431dc6b09d24c1cc7c0
CEGEKANV\alexandb <alexandru.banaru@cegeka.com>
2017-07-21 15:00:31 +0300

    enroll soldiers and find front man

---
+++ Battle/Army.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Battle
+{
+    public class Army
+    {
+        public Army()
+        {
+            Soldiers=new List<Soldier>();
+        }
+        public void AddSoldiers(IList<Soldier> soldiers)
+        {
+           Soldiers.AddRange(soldiers);
+        }
+
+        public List<Soldier> Soldiers { get; }
+
+        public Soldier GetFrontMan()
+        {
+            return Soldiers.FirstOrDefault();
+        }
+    }
+}
